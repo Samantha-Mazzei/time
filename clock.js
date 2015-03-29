4c460dc1cfcd71bb6e23db020604abf7a840fd7a
pierr_000 <pierr_000@192.168.1.17>
2015-03-29 21:13:12 +0200

    - Ajout d'un 0 lorsqu'un nombre est inférieur à 10.

--- clock.js
+++ clock.js
@@ -14,6 +14,15 @@ function displayClock() {
     var hours = today.getHours();
     var minutes = today.getMinutes();
     var seconds = today.getSeconds();
+    if(hours < 10) {
+        hours = '0' + hours;
+    }
+    if(minutes < 10){
+        minutes = '0' + minutes;
+    }
+    if(seconds < 10){
+        seconds = '0' + seconds;
+    }
     // Afficher l'horloge 
     $('.clock').html(hours + ':' + minutes + ':' + seconds);
 }
