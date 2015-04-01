377f753a2970419007d9523ba378cfd562037d65
pierr_000 <pierr_000@192.168.1.17>
2015-04-01 23:26:33 +0200

    - Création d'un countdown.

--- clock.js
+++ clock.js
@@ -4,7 +4,9 @@
 $(document).ready(function() {
     // 
     displayClock();
+    countDown();
     setInterval(displayClock, 1000);
+    setInterval(countDown, 1000);
 });
 
 function displayClock() {
@@ -25,4 +27,31 @@ function displayClock() {
     }
     // Afficher l'horloge 
     $('.clock').html(hours + ':' + minutes + ':' + seconds);
+}
+
+
+function countDown() {
+    // Récupérer la date de l'utisateur
+    var dateString = $('[data-target-date]').attr('data-target-date');
+    var targetDate = new Date(dateString);
+    var today = new Date();
+    var targetTimeStamp = targetDate.getTime();
+    var todayTimeStamp = today.getTime();
+    var resultTimeStamp = targetTimeStamp - todayTimeStamp; 
+    var time = millisecondsToTime(resultTimeStamp);
+}
+
+function millisecondsToTime(milliseconds) {
+    var days = parseInt(milliseconds/1000/60/60/24);
+    milliseconds = milliseconds - days*24*60*60*1000;
+    
+    var hours = parseInt(milliseconds/1000/60/60);
+    milliseconds = milliseconds - hours*60*60*1000;
+    
+    var minutes = parseInt(milliseconds/1000/60);
+    milliseconds = milliseconds - minutes*60*1000;
+    
+    var seconds = parseInt(milliseconds/1000);
+    milliseconds = milliseconds - seconds*1000;
+    console.log(days, hours, minutes, seconds);
 }
